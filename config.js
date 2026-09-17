7a63e04acd1af4585a5af47f8e9ed4d44211035f
chiliverushyam <74700798+chiliverushyam@users.noreply.github.com>
2026-09-17 15:10:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SVVSS_CONFIG={
   SUPABASE_URL:"https://szsqbqcodtlruorcsgpq.supabase.co",
   SUPABASE_ANON_KEY:"sb_publishable_trqs1rQLw0h9QE8HY2275w_Z8IQDFfg",
-  WHATSAPP_NUMBER:""
+  WHATSAPP_NUMBER:"917989816250"
 };
